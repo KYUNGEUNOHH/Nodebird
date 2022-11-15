d5a40250269bb5fc89246ab413053ff3b533c80a
KYUNGEUNOHH <okey2356@gmail.com>
2022-11-15 23:40:59 +0900

    backend

---
+++ prepare/back/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("welcome!");
+});
+
+app.listen(3065, () => {
+  console.log("server on!");
+});
